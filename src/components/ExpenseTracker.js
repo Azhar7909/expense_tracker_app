6e586e587efc4a4b6aa13bbba0d90c32531a6f58
Muhammad Azhar Hussain <44233088+Azhar7909@users.noreply.github.com>
2020-12-08 18:50:46 +0500

    fronte end complete

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -1,67 +1,83 @@
 import React from "react";
 
 export default function ExpenseTracker() {
+  const transactions = [
+    { desc: "Cash", amount: 190 },
+    { desc: "Book", amount: 100 },
+    { desc: "Camera", amount: 10 },
+  ];
+  const date = Date();
+  console.log(date);
   return (
     <div className="container shadow">
       <h2 className="text-center">Expense Tracker App</h2>
-      <h4 className=" pad-15px">
+      <h4 className=" pad-15px text-color">
         Your Balance <br />
-        <span className="font-size-30px">$260.00</span>
+        <span className="font-size-30px darkcyan">$260.00</span>
       </h4>
+      <p className="text-color text-center font-size-12px">{date}</p>
       <div className="flex space-around bg-white shadow">
-        <h5>
+        <h5 className="text-color">
           Income <br />
-          <span className="font-size-20px">$500.00</span>
+          <span className="font-size-20px darkgreen">$500.00</span>
         </h5>
-        <h5>
+        <h5 className="text-color">
           Expense <br />
-          <span className="font-size-20px">$300.00</span>
+          <span className="font-size-20px darkred">$300.00</span>
         </h5>
       </div>
-      <h4 className="pad-bottom-7px hr">Add new description</h4>
+      <h4 className="pad-bottom-7px hr text-color">Add new description</h4>
       <form action="">
-        <label>Enter description or Text</label>
+        <label className="text-color">Enter description or Text</label>
         <br />
         <input
-          className="width-100 pad-7px border-none bg-darkgray out-line-darkcyan"
+          className="width-100 pad-7px border-none bg-darkgray out-line-darkcyan darkcyan"
           type="text"
           required
         />
         <br />
-        <label>Enter amount</label>
+        <label className="text-color ">Enter amount</label>
         <br />
         <input
-          className="width-100 pad-7px border-none bg-darkgray out-line-darkcyan"
-          type="text"
+          className="width-100 pad-7px border-none bg-darkgray out-line-darkcyan darkcyan "
+          type="number"
           required
         />
         <div className="flex space-around pad-15px">
           <div>
-            <input type="radio" />
-            <label htmlFor="">Income</label>
+            <input type="radio" className="curser-pointer"/>
+            <label htmlFor="" className="darkgreen">
+              Income
+            </label>
           </div>
           <div>
-            <input type="radio" />
-            <label htmlFor="">Expense</label>
+            <input type="radio" className="curser-pointer"/>
+            <label htmlFor="" className="darkred">
+              Expense
+            </label>
           </div>
         </div>
         <input
-          className="pad-7px width-100 shadow"
+          className="pad-7px width-100 shadow bg-darkblue color-white curser-pointer"
           type="button"
           value="Add Transaction"
         />
       </form>
-      <h4 className="pad-bottom-7px hr">History</h4>
+      <h4 className="pad-bottom-7px hr text-color">History</h4>
       <ul className="list-style-none">
-          <li className="flex space-between pad-7px shadow bg-white margin-bottom-10px">
-              <span>description</span>
-              <span>amount</span>
-          </li>
-          <li className="flex space-between pad-7px shadow bg-white margin-bottom-10px">
-              <span>description</span>
-              <span>amount</span>
-          </li>
+        {transactions.map((trans, ind) => {
+          return (
+            <li
+              key={ind}
+              className="flex space-between pad-7px shadow bg-white margin-bottom-10px income-border"
+            >
+              <span>{trans.desc}</span>
+              <span>${trans.amount}</span>
+            </li>
+          );
+        })}
       </ul>
+      <p className="text-center text-color">End</p>
     </div>
   );
 }
